ef3a433cc04667a33165eef842bfec6b1bdca9d1
Philippe Chepy <philippe@chepy.eu>
2021-12-13 23:18:24 +0100

    Add README and variable/outputs descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "private_key_pem" {
-  value = tls_private_key.root_ca.private_key_pem
+  description = "The private key of the certificate."
+  value       = tls_private_key.root_ca.private_key_pem
 }
 
 output "certificate_pem" {
-  value = tls_self_signed_cert.root_ca.cert_pem
+  description = "The resulting certificate."
+  value       = tls_self_signed_cert.root_ca.cert_pem
 }
--- variables.tf
+++ variables.tf
@@ -1,25 +1,27 @@
 variable "key_algorithm" {
+  description = "Key Algorithm (e.g. 'RSA' or 'ECDSA'), with 'RSA' as the default."
   type        = string
   default     = "RSA"
-  description = "Key Algorithm (e.g. 'RSA' or 'ECDSA')"
 }
 
 variable "ecdsa_curve" {
+  description = "May be any of 'P224', 'P256', 'P384' or 'P521', with 'P224' as the default."
   type        = string
   default     = null
-  description = "May be any of 'P224', 'P256', 'P384' or 'P521', with 'P224' qs the default"
 }
 
 variable "rsa_bits" {
+  description = "Defaults to '4096' bits."
   type        = string
-  default     = null
-  description = "Defaults to 2048"
+  default     = 4096
 }
 
 variable "common_name" {
-  type = string
+  description = "Define the certificate common name."
+  type        = string
 }
 
 variable "validity_period_hours" {
-  type = number
+  description = "The certificate will expire after this amount of time."
+  type        = number
 }
